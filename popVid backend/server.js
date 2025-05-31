426b647bab2af5d4008d78c949ab8380603d8e00
Parampreet Singh <params@uvic.ca>
2025-05-30 21:28:05 -0600

    backend finish

--- popVid backend/server.js
+++ popVid backend/server.js
@@ -9,6 +9,11 @@ const port = 9000;
 
 // middlewares
 app.use(express.json())
+app.use((res, res, next)=>{
+    res.setHeaders('Access-Control-Allow-Origin', '*'),
+    res.setHeaders('Access-Control-Allow-Headers', '*'),
+    next()
+})
 // db config
 const connection_url = "mongodb+srv://paramsbhs2:Grewal123@popvid.culuc1m.mongodb.net/?retryWrites=true&w=majority&appName=popVid";
 
@@ -16,15 +21,23 @@ mongoose.connect(connection_url)
 // api endpoints
 app.get('/', (req,res) => res.status(200).send("Hello World"));
 app.get('/v1/posts', (req,res) => res.status(200).send(Data));
-app.get('/v2/posts', (req,res) => {
-    const dbVideos = req.body
-    Videos.create(dbVideos, (err, data)=>{
-        if(err){
-            res.status(500).send(err)
-        }else{
-            res.status(201).send(data)
-        }
-    })
+app.get('/v2/posts', async (req, res) => {
+    try {
+        const data = await Videos.find({});
+        res.status(200).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+app.post('/v2/posts', async (req, res) => {
+    const dbVideos = req.body;
+
+    try {
+        const data = await Videos.create(dbVideos);
+        res.status(201).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 // listen
 app.listen(port, ()=>console.log(`listening on localhost: ${port}`));
